e630262846ef7f8afb6674925704a73ad20348ec
dongji22 <94667489+dongji22@users.noreply.github.com>
2026-06-19 12:32:08 +0800

    Create s.js

---
+++ s.js
@@ -0,0 +1,145 @@
+
+{
+  
+
+    "sites": [
+        
+        
+{"key":"DJ-Z-量子","name":"🌴量子┃直连Z","type":1,"api":"https://cj.lziapi.com/api.php/provide/vod","searchable":1,"categories": ["大陆综艺", "国产剧", "韩国剧", "欧美剧", "恐怖片","喜剧片","动作片","科幻片","爱情片", "国产动漫","电影解说","伦理片","港台综艺","纪录片"]},
+{"key":"DJ-Z-暴风","name":"️🌴暴风┃直连Z","type":1,"api":"https://bfzyapi.com/api.php/provide/vod/","searchable":1,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-非凡","name":"️🌴非凡┃直连Z","type":1,"api":"https://api.ffzyapi.com/api.php/provide/vod","searchable":1,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+
+{"key":"DJ-Z-虎牙","name":"️🌴虎牙┃直连Z","type":1,"api":"https://www.huyaapi.com/api.php/provide/vod","searchable":1
+,"categories":["综艺","内地剧","记录片","韩剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-优质","name":"️🌴优质┃直连Z","type":1,"api":"https://api.yyzy-tv.vip/inc/apijson.php?ac=list","searchable":1
+	,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-红牛","name":"️🌴红牛┃直连Z","type":1,"api":"https://www.hongniuzy2.com/api.php/provide/vod","searchable":1
+	,"categories":["综艺","国产剧","记录片","韩剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-鸭鸭","name":"️🌴鸭鸭┃直连Z","type":1,"api":"https://cj.yayazy.net/api.php/provide/vod/?ac=list","searchable":1
+	,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-牛牛","name":"️🌴牛牛┃直连Z","type":1,"api":"https://api.niuniuzy.me/api.php/provide/vod","searchable":1
+	,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+{"key":"DJ-Z-360","name":"️🌴360┃直连Z","type":1,"api":"https://360zyzz.com/api.php/provide/vod","searchable":1
+	,"categories":["大陆综艺","国产剧","国产动漫","记录片","韩国剧","香港剧","台湾剧","欧美剧","动作片","科幻片","战争片","奇幻片","喜剧片","爱情片","恐怖片","犯罪片","悬疑片","惊悚片","剧情片","冒险片"]},
+
+{"key":"DJ-Z-索尼","name":"🌴索尼┃直连Z","type":1,"api":"https://suoniapi.com/api.php/provide/vod/?ac=list","searchable":1,"categories": ["大陆综艺", "国产剧", "韩国剧", "欧美剧", "恐怖片","喜剧片","动作片","科幻片","爱情片","动画片", "国产动漫","港台综艺","纪录片"]},
+{"key":"DJ-Z-无尽","name":"🌴无尽┃直连Z","type":1,"api":"https://api.wujinapi.me/api.php/provide/vod/?ac=list","searchable":1,"categories":["大陆综艺","动画片","动作片","喜剧片","爱情片","科幻片","恐怖片","剧情片","战争片","悬疑片","犯罪片","奇幻片","记录片","内地剧","香港剧","台湾剧","日剧","韩剧","欧美剧","马泰剧","港台综艺","日韩综艺","欧美综艺","伦理片","中国动漫","日本动漫","欧美动漫"]},
+
+{"key":"DJ-Z-火狐","name":"🌴火狐┃直连Z","type":1,"api":"https://hhzyapi.com/api.php/provide/vod/?ac=list","searchable":1,"categories":["大陆综艺","动画片","动作片","喜剧片","爱情片","科幻片","恐怖片","剧情片","战争片","悬疑片","犯罪片","奇幻片","记录片","内地剧","香港剧","台湾剧","日剧","韩剧","欧美剧","马泰剧","港台综艺","日韩综艺","欧美综艺","伦理片","中国动漫","日本动漫","欧美动漫"]},
+{"key":"DJ-Z-极速","name":"🌴极速┃直连Z","type":1,"api":"https://jszyapi.com/api.php/provide/vod/from/jsm3u8/","searchable":1,"categories":["大陆综艺","内地剧","中国动漫","香港剧","动作片","爱情片","恐怖片","剧情片","战争片","灾难片","悬疑片","犯罪片","奇幻片","纪录片","体育赛事","动漫","动画片","日本动漫","欧美动漫","欧美剧","韩剧","日剧","马泰剧","台湾剧","综艺","日韩综艺","港台综艺","欧美综艺"]},
+{"key":"DJ-Z-糊鸭","name":"🌴糊鸭┃直连Z","type":1,"api":"https://www.huyaapi.com/api.php/provide/vod/","searchable":1,"categories": ["综艺", "国产剧", "动画片", "欧美剧", "香港剧", "韩剧", "日剧", "马泰剧", "恐怖片","喜剧片","动作片","科幻片","爱情片","纪录片", "国产动漫","伦理片","港台综艺"]},
+{"key":"DJ-Z-豪华","name":"🌴豪华┃直连Z","type":1,"api":"https://hhzyapi.com/api.php/provide/vod","searchable":1,"categories":["动画片","中国动漫","日本动漫","欧美动漫","剧情片","战争片","动作片","科幻片","记录片","爱情片","喜剧片","灾难片","悬疑片","犯罪片","内地剧","香港剧","欧美剧","韩剧","日剧","马泰剧","台湾剧","大陆综艺","日韩综艺","港台综艺","欧美综艺"]},
+
+        
+        
+        
+        {
+            "key": "push_agent",
+            "name": "📤 推送播放",
+            "type": 3,
+            "filterable": 0,
+            "changeable": 0,
+            "api": "csp_Push",
+            "searchable": 1,
+            "quickSearch": 1,
+            "ext": ""
+        }
+    ],
+    "parses": [
+        {
+            "name": "📦 Json聚合",
+            "type": 3,
+            "url": "Demo"
+        },
+        {
+            "name": "🎬 影视聚合",
+            "type": 3,
+            "url": "影视"
+        }
+    ],
+    "rules": [
+        {
+            "name": "农民",
+            "hosts": [
+                "toutiaovod.com"
+            ],
+            "regex": [
+                "video/tos/cn"
+            ]
+        },
+        {
+            "name": "火山",
+            "hosts": [
+                "huoshan.com"
+            ],
+            "regex": [
+                "item_id="
+            ]
+        },
+        {
+            "name": "抖音",
+            "hosts": [
+                "douyin.com"
+            ],
+            "regex": [
+                "is_play_url="
+            ]
+        },
+        {
+            "name": "磁力广告",
+            "hosts": [
+                "magnet"
+            ],
+            "regex": [
+                "更多",
+                "请访问",
+                "example",
+                "社 區",
+                "x u u",
+                "直 播",
+                "更 新",
+                "社 区",
+                "有趣",
+                "有 趣",
+                "英皇体育",
+                "全中文AV在线",
+                "澳门皇冠赌场",
+                "哥哥快来",
+                "美女荷官",
+                "裸聊",
+                "新片首发",
+                "UUE29"
+            ]
+        },
+        {
+            "name": "饭团点击",
+            "hosts": [
+                "dadagui",
+                "freeok",
+                "dadagui"
+            ],
+            "script": [
+                "document.querySelector(\"#playleftiframe\").contentWindow.document.querySelector(\"#start\").click();"
+            ]
+        },
+        {
+            "name": "毛驴点击",
+            "hosts": [
+                "www.maolvys.com"
+            ],
+            "script": [
+                "document.getElementsByClassName('swal-buttonswal-button--confirm')[0].click()"
+            ]
+        }
+    ],
+
+    "lives": [
+            {"name":"li","type":0,"url":"https://li.dj2.ccwu.cc/abc123","playerType":2},
+
+
+{"name":"hacks","type":0,"url":"https://live.hacks.tools/tv/iptv4.txt"},
+{"name":"izbds","type":0,"url":"https://live.zbds.top/tv/iptv4.txt"},
+{"name":"izbds6","type":0,"url":"https://live.zbds.top/tv/iptv6.txt"}
+
+    ]
+}
